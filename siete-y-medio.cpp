2b5faf173bc4dad6dff8c100c46d36fb139f20b1
simbar13 <64224771+simbar13@users.noreply.github.com>
2020-04-25 11:00:29 -0700

    Preliminary version, more detail and bug fixes ahead

--- siete-y-medio.cpp
+++ siete-y-medio.cpp
@@ -17,10 +17,12 @@ using namespace std;
 
 
 // Stub for main
-int main(){
-   /* --STATEMENTS-- */
+int main() {
+	/* --STATEMENTS-- */
 	int dealer_negative = 0;
 	Player* user = new Player(100);
+	Player* dealer = new Player(900);
+	bool onwards = true;
 	while ((user->money_left() > 0) && (dealer_negative < 900))
 	{
 		//here is where all the game play happens
@@ -34,6 +36,7 @@ int main(){
 			cout << "You cannot bet more money than you have"<<endl;
 			continue;
 		}
+
 		string add_card = "yes";
 		Card* first = new Card();
 		user->deal(first);
@@ -52,23 +55,78 @@ int main(){
 				cout << endl << "Your total point value is:";
 				cout << user->my_hand()->total_value();
 			}
-			//if the total hand value is less than 7.5:
-			cout << endl << "Would you like another card? (y/n)";
-			char more;
-			cin >> more;
-			if (more == 'n')
+			if (user->my_hand()->total_value() <= 7.5)
 			{
-				add_card = "no";
-				continue;
+				cout << endl << "Would you like another card? (y/n)";
+				char more;
+				cin >> more;
+				if (more == 'n')
+				{
+					add_card = "no";
+					continue;
+				}
+				//now it is determined that the player would indeed like a new card
+				Card* c = new Card();
+				user->deal(c);
 			}
-			//now it is determined that the player would indeed like a new card
-			Card* c = new Card();
-			user->deal(c);
-			//now if the total hand value is not less than 7.5:
-			//cout something like youve reeached 7.5 you dont draw anymore
+			else
+			{
+				cout << endl<<"You busted, the dealer wins!" << endl;
+				add_card = "no";
+				onwards = false;
+				user->alter(player_bet, false);
+				dealer->alter(player_bet, true);
+
+			}
 			
 		}
+		if (!onwards)
+			continue;
+		Card* dfirst = new Card();
+		dealer->deal(dfirst);
+		cout << "The Dealer's first card is:";
+		dealer->my_hand()->listCards();
+		cout << endl << "The Dealer's initial total is:";
+		cout << dealer->my_hand()->total_value();
+		if (dealer->my_hand()->total_value() < 5.5)
+		{
+			while (dealer->my_hand()->total_value() < 5.5)
+			{
+				cout << endl << "The Dealer will draw, press C followed by enter to continue";
+				char hi;
+				cin >> hi;
+				Card* d = new Card();
+				dealer->deal(d);
+				cout << "The Dealer's hand contains:";
+				dealer->my_hand()->listCards();
+				cout << endl << "The Dealer's new total is:";
+				cout << dealer->my_hand()->total_value();
+
+			}
+		}
+		if (dealer->my_hand()->total_value() > 7.5)
+		{
+			cout << endl << "The Dealer busted, you win!" << endl;
+			user->alter(player_bet, true);
+			dealer->alter(player_bet, false);
+		}
+		else if (dealer->my_hand()->total_value() > user->my_hand()->total_value())
+		{
+			cout << endl << "The Dealer wins!" << endl;
+			user->alter(player_bet, false);
+			dealer->alter(player_bet, true);
+		}
+		else
+		{
+			cout << endl << "You win!" << endl;
+			user->alter(player_bet, true);
+			dealer->alter(player_bet, false);
+		}
+		//here we empty the players hand to prepare for the next round
+		user->my_hand()->reset();
+		dealer->my_hand()->reset();
 
 	}
+
 	return 0;
 }
